35c55df233762518a3eaa74bc5475853ba8ef1c2
David Isaksson <davidisaksson93@gmail.com>
2017-11-19 11:53:37 +0100

    Added hdpi support

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+
     QApplication a(argc, argv);
 
     a.setApplicationName("StretchTimer");
