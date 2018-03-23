aa299f739c77d63e55ad8e1551eafae68dbdf656
David Isaksson <davidisaksson93@gmail.com>
2018-03-23 22:46:10 +0100

    Renamed project from StretchTimer to stretch-timer

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
 
-    a.setApplicationName("StretchTimer");
+    a.setApplicationName("stretch-timer");
     a.setOrganizationName("StretchTimer");
     a.setWindowIcon(QIcon("://icon.ico"));
 
--- src/stretch-timer.pro
+++ src/stretch-timer.pro
@@ -2,7 +2,7 @@ QT       += core gui multimedia
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-TARGET = StretchTimer
+TARGET = stretch-timer 
 TEMPLATE = app
 
 win32:LIBS += -luser32
