a22cacbdb807652318c4a3f05e051600db2e393d
David Isaksson <davidisaksson93@gmail.com>
2021-04-09 16:07:19 +0200

    Add build script for deb packaging

---
+++ scripts/build-deb.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+BUILD_DIR="build-deb"
+
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
+cmake --build . -- package -j4
